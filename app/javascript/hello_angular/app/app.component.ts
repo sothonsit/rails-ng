959e5a216685fa63ad69e936df39a3cd66a0bdb8
Jester <jester@Jesters-MacBook-Pro.local>
2019-01-23 15:35:18 +0700

    remove input angular

--- app/javascript/hello_angular/app/app.component.ts
+++ app/javascript/hello_angular/app/app.component.ts
@@ -2,14 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hello-angular',
-  template: `<h1>Hello {{ hero }}</h1>`
+  template: `<h1>Hello</h1>`
 })
 export class AppComponent implements OnInit {
 
-  @Input('hero') hero: any;
-
   ngOnInit(): void {
-    console.log(this.hero);
   }
 
 }
